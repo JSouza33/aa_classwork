655df55af8b6d12ee0a34defac2a001150a64b33
Justin Souza <justinsouza1029@gmail.com>
2020-12-07 18:24:05 -0800

    Added move method

--- W4D1/tic_tac_toe/skeleton/skeleton/lib/super_computer_player.rb
+++ W4D1/tic_tac_toe/skeleton/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,13 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToe.new(game.board, mark)
+    node.children = node.children
+    node =  node.children.find { |child| child.winning_node?(mark) }
+    return node.prev_move_pos if node 
+    node =  node.children.find { |child| !child.losing_node?(mark) }
+    return node.prev_move_pos if node
+    raise "prepare for being a loser"
   end
 end
 
@@ -11,4 +18,5 @@ if __FILE__ == $PROGRAM_NAME
   cp = SuperComputerPlayer.new
 
   TicTacToe.new(hp, cp).run
-end
+end 
+
