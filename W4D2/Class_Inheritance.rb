68113c0afa86144862fc32fc6cda9cfdb95d5da3
James Koenig <Koenig.James@Live.com>
2020-12-08 16:06:05 -0800

    Finished up Employee class and did Manager outline
    did a brief unit test for Employee.  Manager is currently broken and
    will throw errors if Manager.new is run, lol

--- W4D2/Class_Inheritance.rb
+++ W4D2/Class_Inheritance.rb
@@ -1,9 +1,13 @@
 
 
-
-
-class Employee #(name,title,salary,boss)
-    attr_reader :name, :title, :salary, :boss 
+class Employee 
+    attr_reader :name, :title, :salary, :boss
+    def initialize(name,title,salary,boss)
+        @name   = name
+        @title  = title
+        @salary = salary
+        @boss   = boss
+    end
     def bonus(multiplier) 
         salary * multiplier
     end
@@ -12,12 +16,26 @@ end
 
 
 class Manager < Employee
-    attr_reader :employees 
-
+    attr_reader :employees
+    def initialize
+        super()
+    end 
     def bonus(multiplier)
-        employees_bonuses = employees.map(&:bonus) # Makes an array of all sub-employees bonuses 
-        
-
+        employees__bonuses = employees.map { |employee| employee.bonus(multiplier) }  # Makes an array of all sub-employees bonuses 
+        return employees_bonuses.sum
     end
 
+end
+
+# ned    =  Manager.new(   "Ned",    "Founder", 1_000_000,    nil)
+# darren =  Manager.new("Darren", "TA Manager",    78_000,    ned)
+# shawna = Employee.new("Shawna",         "TA",    12_000, darren)
+david  = Employee.new( "David",         "TA",    10_000, nil)
+
+if __FILE__ == $PROGRAM_NAME
+    p __FILE__
+    p $PROGRAM_NAME
+    # p ned.bonus(5)
+    # p darren.bonus(4)
+    p david.bonus(3)
 end
