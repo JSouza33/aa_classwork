c3f9b2e629bc11e0c398ba00a2138937148963fe
Itai Farhi <itaimf@gmail.com>
2020-12-17 15:21:08 -0800

    Write associations for enrollment and user

--- W5D4/skeleton/skeleton/app/models/course.rb
+++ W5D4/skeleton/skeleton/app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-    has_many: :enrollments, 
+    has_many :enrollments, 
         primary_key: :id, 
         foreign_key: :course_id, 
         class_name: :Enrollment    
